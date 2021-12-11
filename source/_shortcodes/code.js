d36c4c54e0747840016497bf66911ce995515f6a
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-12-11 15:44:40 -0500

    add more code arguments

--- source/_shortcodes/code.js
+++ source/_shortcodes/code.js
@@ -1,5 +1,7 @@
-module.exports = function( content, lines ) {
+module.exports = function( content, language, id, lines ) {
+  const languageClass = language ? ` class="language-${ language }"` : '';
+  const dataId = id ? ` data-id=${ id }` : '';
   const linesContent = lines ? `="${ lines }"` : '';
 
-  return `<pre><code data-trim data-line-numbers${ linesContent }>${ content }</code></pre>`;
+  return `<pre${ dataId }><code${ languageClass } data-trim data-line-numbers${ linesContent }>${ content }</code></pre>`;
 };
