8e58dc51ece4a624e71fc61983858720a8cc9534
Reed Piernock <code@reedcodes.com>
2023-03-31 18:43:26 -0400

    update image shortcode to allow for gifs

--- source/_config/shortcodes/image.js
+++ source/_config/shortcodes/image.js
@@ -30,9 +30,12 @@ module.exports = async function( src, alt, link ) {
   // Set the metadata for this image, that 11ty will generate.
   const metadata = await Image(imageSrc, {
     widths: [300, 600, 900, 1200],
-    formats: ['webp', 'jpeg'],
+    formats: ['auto'],
     outputDir: `${outputDirectory}/images`,
     urlPath: `${this.page.url}/images`,
+    sharpOptions: {
+      animated: true
+    },
     filenameFormat: function (id, imageSrc, width, format, options) {
       const extension = path.extname(imageSrc);
       const name = path.basename(imageSrc, extension);
@@ -41,12 +44,11 @@ module.exports = async function( src, alt, link ) {
     }
   });
 
-  const lowsrc = metadata.jpeg[0];
-  const highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+  const imageData = src.includes('.gif') ? metadata.gif[metadata.gif.length - 1] : metadata.jpeg[metadata.jpeg.length - 1];
 
   const picture = `<picture>${Object.values(metadata).map(imageFormat => {
     return `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(', ')}" sizes="100vw">`;
-  }).join('')}<img src="${lowsrc.url}" width="${highsrc.width}" height="${highsrc.height}" alt="${alt}" loading="lazy" decoding="async"></picture>`;
+  }).join('')}<img src="${imageData.url}" width="${imageData.width}" alt="${alt}" loading="lazy" decoding="async"></picture>`;
 
   const image_content = link ? `<a href="${link}">${picture}</a>` : picture;
 
