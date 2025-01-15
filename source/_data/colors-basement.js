113d1bb346c3f954977d5fad41a68fc4a9b93053
Reed Piernock <agile.plan2042@snooble.com>
2025-01-14 21:02:45 -0500

    add basement color theme

---
+++ source/_data/colors-basement.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    // Caramel.
+    name: "primary",
+    color: "#E18135"
+  },
+  {
+    // Saffron.
+    name: "secondary",
+    color: "#EDC55E"
+  },
+  {
+    // Alice blue.
+    name: "tertiary",
+    color: "#D7E1EA"
+  },
+  {
+    // Black bean.
+    name: "background",
+    color: "#351A11"
+  },
+  {
+    // Champagne.
+    name: "foreground",
+    color: "#F8E7DD"
+  }
+];
